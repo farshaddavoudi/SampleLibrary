d1a85c385e11e28b0f953e8375b0d190477c9cd2
Farshad Davoudi <f.davoudi.r@outlook.com>
2020-12-26 03:29:58 +0330

    Add Blazored.Modal #Front

---
+++ Client/Web/ATA.Library.Client.Web.UI/Infrastructure/Modal/ModalInstaller.cs
@@ -0,0 +1,15 @@
+﻿using ATA.Library.Client.Web.Service.AppSetting;
+using Blazored.Modal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ATA.Library.Client.Web.UI.Infrastructure.Modal
+{
+    public class ModalInstaller : IServiceInstaller
+    {
+        public void InstallServices(IServiceCollection services, AppSettings appSettings)
+        {
+            // https://github.com/Blazored/Modal
+            services.AddBlazoredModal();
+        }
+    }
+}
