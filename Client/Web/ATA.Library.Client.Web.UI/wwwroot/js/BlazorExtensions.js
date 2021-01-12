186c94cda0caf20845d09884d68f240572dd89ae
Farshad Davoudi <f.davoudi.r@outlook.com>
2021-01-12 11:42:22 +0330

    Add linkedIn and log #Log

--- Client/Web/ATA.Library.Client.Web.UI/wwwroot/js/BlazorExtensions.js
+++ Client/Web/ATA.Library.Client.Web.UI/wwwroot/js/BlazorExtensions.js
@@ -48,6 +48,7 @@ function addClassToElementById(elementId, className) {
 // My signature in console
 function addSignature() {
     console.log('%c Developed by %cF@rsh@d D@voudi', 'font-weight:bold; font-size:25px; color: blue;', 'font-weight:bold; font-size:25px;letter-spacing:3px;color:green;text-shadow:1px 1px 0px black, 1px -1px 0px black, -1px 1px 0px black, -1px -1px 0x black;');
+    console.log('contact me: https://www.linkedin.com/in/farshad-davoudi/');
 }
 
 // Clear console
