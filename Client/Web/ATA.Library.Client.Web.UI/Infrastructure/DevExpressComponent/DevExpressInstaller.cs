04de80199808deb9318a65c07a17d399dc34fe89
Farshad Davoudi <f.davoudi.r@outlook.com>
2021-01-15 22:10:03 +0330

    DevExpress component added to project #0

---
+++ Client/Web/ATA.Library.Client.Web.UI/Infrastructure/DevExpressComponent/DevExpressInstaller.cs
@@ -0,0 +1,13 @@
+﻿using ATA.Library.Client.Web.Service.AppSetting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ATA.Library.Client.Web.UI.Infrastructure.DevExpressComponent
+{
+    public class DevExpressInstaller : IServiceInstaller
+    {
+        public void InstallServices(IServiceCollection services, AppSettings appSettings)
+        {
+            services.AddDevExpressBlazor();
+        }
+    }
+}
